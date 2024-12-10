515a45f842aa80e568010edd89c64a4df7b13f23
Artem Zhikharev <artemziharew@gmail.com>
2024-12-10 22:37:26 +0300

    add json to init data, add controller

---
+++ DigitalWater.Api/Api/ExternalApi/v1/SensorController.cs
@@ -0,0 +1,20 @@
+﻿using DigitalWater.Api.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalWater.Api.Api.ExternalApi.v1;
+
+/// <summary>
+/// Контроллер для работы с датчиками
+/// </summary>
+[ApiController]
+[ApiVersion("1.0")]
+[SetRoute]
+public class SensorController : ControllerBase
+{
+    /// <summary>
+    /// Пинг
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("hello")]
+    public IActionResult Ping() => Ok("Hello");
+}
